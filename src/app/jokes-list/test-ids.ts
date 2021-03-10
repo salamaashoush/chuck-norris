98f4b0f9ef86afe459b5145d4538f53f981fa3d8
Salama Ashoush <salamaashoush@gmail.com>
2021-03-10 14:34:32 +0100

    add mising test ids

--- src/app/jokes-list/test-ids.ts
+++ src/app/jokes-list/test-ids.ts
@@ -1,7 +1,8 @@
 import { IJoke } from '../types';
 
 export const JOKES_LIST_TEST_IDS = {
-  addToFavourites: (joke: IJoke) => `${joke.id}-add-to-favourites`,
-  removeFromFavourites: (joke: IJoke) => `${joke.id}-remove-from-favourites`,
-  joke: (joke: IJoke) => `${joke.id}-joke`,
+  addToFavourites: (joke: Partial<IJoke>) => `${joke.id}-add-to-favourites`,
+  removeFromFavourites: (joke: Partial<IJoke>) => `${joke.id}-remove-from-favourites`,
+  joke: (joke: Partial<IJoke>) => `joke-${joke.id}`,
+  jokesList: 'jokes-list',
 };
--- src/app/test-ids.ts
+++ src/app/test-ids.ts
@@ -2,3 +2,6 @@ export const APP_TEST_IDS = {
   refresh: 'app-refresh',
   getRandomJoke: 'get-random-joke',
 };
+
+export * from './favourites-list/test-ids';
+export * from './jokes-list/test-ids';
